c51c091e796f45f2132335e077f183feca7f3f48
faisalsayed10 <faisal.sayed502@gmail.com>
2022-04-09 15:47:33 +0530

    Render price and change of currency

---
+++ src/Price.js
@@ -0,0 +1,25 @@
+import { Image, Text, Box } from "@mantine/core";
+import React from "react";
+import { TrendingDown, TrendingUp } from "tabler-icons-react";
+
+const Price = ({ coin, increased }) => {
+	return (
+		<Box sx={{ display: "flex", alignItems: "center" }}>
+			<Image src={coin.image} alt={coin.name} width="32px" mr="5px" />
+			<Text size="xl" mr="xs">
+				{coin.symbol.toUpperCase()} - ${parseFloat(coin.current_price.toFixed(3))}
+			</Text>
+			<Box
+				sx={{ display: "flex", alignItems: "center" }}
+				className={increased ? "increased" : "decreased"}
+			>
+				{increased ? <TrendingUp /> : <TrendingDown />}
+				<Text size="sm" ml="5px">
+					{coin.price_change_percentage_24h.toFixed(2)}%
+				</Text>
+			</Box>
+		</Box>
+	);
+};
+
+export default Price;
