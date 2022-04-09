57241581de4cbdfa70c88f2c2a89d28a9c5a548c
faisalsayed10 <faisal.sayed502@gmail.com>
2022-04-09 15:47:22 +0530

    Style changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
 	<React.StrictMode>
-		<App />
+		<MantineProvider>
+			<NotificationsProvider>
+				<App />
+			</NotificationsProvider>
+		</MantineProvider>
 	</React.StrictMode>
 );
